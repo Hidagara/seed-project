55030b867314757c23915403452468b01a7eb3a2
hidagara <hidagara@gmail.com>
2017-09-02 17:33:19 +0300

    token support for auth users

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,18 +1,30 @@
 import {Component} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MessageService} from "../message/message.service";
+import {User} from "./user.model";
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 @Component ({
     selector: 'app-signin',
     templateUrl:'/signin.component.html'
 })
 export class  SigninComponent{
 
-    constructor(private messageService: MessageService){};
+    constructor(private authService: AuthService,private router: Router){};
 
     myForm: FormGroup;
 
     onSubmit(){
-        console.log(this.myForm);
+        const user = new User(this.myForm.value.email,this.myForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token',data.token);
+                    localStorage.setItem('userId',data.userId);
+                    this.router.navigateByUrl('/')
+                },
+                error => console.log(error)
+            )
         this.myForm.reset();
     }
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -8,7 +9,7 @@ router.post('/',function(req,res,next){
     var user = new User({
         firstName:  req.body.firstName,
         lastName:  req.body.lastName,
-        password:  req.body.password,//bcrypt.hashSync(req.body.password,10),
+        password: bcrypt.hashSync(req.body.password,10),
         email:    req.body.email
     });
 
@@ -25,6 +26,37 @@ router.post('/',function(req,res,next){
             obj: result
         });
     });
+})
+
+router.post('/signin',function (req,res,next) {
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            })
+        }
+        if (!user){
+            return res.status(500).json({
+                title: 'Cant find user',
+                error: {message:'Invalid credentials'}
+            });
+        }
+        if (!bcrypt.compareSync(req.body.password,user.password)){
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            })
+        }
+
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        })
+
+    })
 })
 
 module.exports = router;
