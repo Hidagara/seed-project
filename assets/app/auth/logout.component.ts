f5aa37f7f828c030aa82b81c5ae34fb000c013ec
hidagara <hidagara@gmail.com>
2017-09-02 19:35:54 +0300

    token support for auth users

--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -1,5 +1,7 @@
 
 import {Component} from "@angular/core";
+import {Router} from "@angular/router";
+import {AuthService} from "./auth.service";
 
 @Component({
 
@@ -13,7 +15,10 @@ import {Component} from "@angular/core";
     `
 })
 export class LogoutComponent{
-    onLogout(){
 
+    constructor (private authService: AuthService, private router: Router){}
+    onLogout(){
+            this.authService.logout();
+            this.router.navigate(['/auth','signin']);
     }
 }
